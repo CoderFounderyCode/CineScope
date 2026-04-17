ce4415b27a1a0111239467e810b477524feb87f7
John Maguire <31592841+johnpmaguire@users.noreply.github.com>
2026-04-17 20:38:14 +0100

    Video 11 min 20
    Add TMDBService for movie API integration
    
    Introduced TMDBService to handle TMDB API requests, including API key setup and fetching "now playing" movies. Registered the service for DI in Program.cs and updated _Imports.razor for service accessibility in components.

---
+++ Services/TMDBService.cs
@@ -0,0 +1,51 @@
+﻿using CineScope.Models;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+
+namespace CineScope.Services
+{
+    public class TMDBService
+    {
+        private readonly HttpClient _http;
+
+        public TMDBService(HttpClient _http, IConfiguration config)
+        {
+            _http = _http;
+
+            string tmdbApiKey = config["TmdbAccessKey"];
+            if (!string.IsNullOrEmpty(tmdbApiKey))
+            {
+                _http.DefaultRequestHeaders.Add("Authorization", $"Bearer {tmdbApiKey}");
+            }
+        }
+    }
+
+    public async Task<MovieListResponse> GetNowPlayingMovies()
+        {
+            string apiUrl = $"https://api.themoviedb.org/3/movie/now_playing?region=GB&languages=en";
+            string imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+
+            MovieListResponse response = await _http.GetFromJsonAsync<MovieListResponse>(apiUrl)
+                 ?? throw new HttpIOException(HttpRequestError.InvalidResponse, "Failed to load now playing movies");
+
+
+            // Results is the property that contains the list of movies in the response from the class MovieListResponse
+            foreach (var movie in response.Results)
+            {
+                if (string.IsNullOrEmpty(movie.PosterPath))
+                {
+                    movie.PosterPath = "/images/poster.png";
+                }
+                else
+                {
+                    movie.PosterPath = $"{imageBaseUrl}{movie.PosterPath}";
+                }
+            }
+            return response;
+
+
+        }
+
+    }
+}
